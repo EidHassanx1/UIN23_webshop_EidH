49fb19465073d1f1bf0b5163edc093374699b30d
EidHassanx1 <eid-hassan@outlook.com>
2023-01-19 13:58:47 +0100

    ..

---
+++ intro/shopscript.js
@@ -0,0 +1,28 @@
+let cart = []
+
+function addToCart(title, price) {
+    cart.push({productTitle: title, productPrice: price, productQuantity: 1})
+    console.log(cart)
+    //Etter å ha lagt til et produkt; oppdater handlelistevisningen
+    renderCart()
+    //Så nå må vi oppdatere label med antall produkter:
+    document.querySelector("#cart .label"). innerHTML = cart.length
+}
+
+function renderCart() {
+    //Tom variabel for å bygge HTML til produkter
+    let listHTML
+    //Gå gjennom cart-arrayen, lag <li> for hvert produkt
+    cart.map(prod => listHTML += `<li>
+    <span class="title">${prod.productTitle}</span>
+    <span class="price">${prod.productPrice}</span>
+    <span class="quantity">${prod.productQuantity}</span>
+    <button class="delete">X</button>
+</li>`)
+    //Bruke en selector for å kunne finne riktig <ul>, og skrive inn listHMTL:
+    document.querySelector("#cartview ul").innerHTML = listHTML;
+}
+
+function toggleCart() {
+    document.querySelector("#cartview").classList.toggle("hidden")
+}
